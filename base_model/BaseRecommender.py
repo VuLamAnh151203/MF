53a7ca302ee3f9339ad838b56b470535b8711faa
Lamanh1234 <vulamanhbk2003@gmail.com>
2026-01-16 13:57:09 +0700

    fix

--- base_model/BaseRecommender.py
+++ base_model/BaseRecommender.py
@@ -162,7 +162,7 @@ class BaseColdStartTrainer(object):
         print(f'Evaluating the model under the {valid_type} setting...')
         rec_list = self.valid(valid_type)
         # print(rec_list.keys())
-        print(valid_set[43][:20])
+        print(valid_set[43].keys())
         measure, _ = ranking_evaluation(valid_set, rec_list, [self.max_N])
         if len(self.bestPerformance) > 0:
             count = 0
